cf5a4520be160a2309bacaa61471f90031097be2
Maria Alice <alicedflopes.02@gmail.com>
2024-05-30 15:46:28 -0300

    feat: add a function to create a new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-// import { useState } from 'react'
+import { useState } from 'react'
 
 // Validation Lib
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -29,12 +29,15 @@ const newCycleFormValidationSchema = zod.object({
 
 // Types
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 
 export function Home() {
-  // const [task, setTask] = useState('')
-  // function resetForm() {
-  //   setTask('')
-  // }
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -45,10 +48,19 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(newCycle.id)
+
     reset()
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const taskContent = watch('task')
   const isSubmitDisabled = !taskContent
 
